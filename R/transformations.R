9bbeae1cf8f43bf81925ad9309ac9be5399aa062
Mitchell Lisle <mitchell.lisle@bigdatr.com>
2017-10-25 11:25:38 +1100

    added census transform

--- R/transformations.R
+++ R/transformations.R
@@ -1,11 +1,11 @@
 #' List 2 Test
 #' @description Takes a column that is made up of a list and converts to
 #' concatenated values with a separator specified. Defaults to comma.
-#' 
+#'
 #' @param column the column to transform
 #' @param sep the separator to use
-#' 
-dm_lst2txt <- function (column, sep = ", ") 
+#'
+dm_lst2txt <- function (column, sep = ", ")
 {
   loadNamespace("stringr")
   ret <- sapply(column, function(x) {
@@ -29,4 +29,17 @@ dm_strclean <- function(words) {
   words <- stringr::str_replace_all(words, " +", " ")
   words <- stringr::str_replace_all(words, "\\.\\.+", ".")
   words <- stringr::str_trim(words)
+}
+
+#' Census Reshape
+#'  @description The census data from ABS typically comes in a wide format. This function will
+#' convert it into a long format as it's often easier to work with and fit for more
+#' purposes.
+#'
+#' @param
+census_reshape <- function(data, id){
+  molten <- melt(data, id = id, na.rm = FALSE)
+  result <- data.frame(molten)
+
+  return(result)
 }
